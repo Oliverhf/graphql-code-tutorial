f8f05ae80afb6bab2ae772ab08c0e7119e1b1d47
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-06-07 14:23:09 -0300

    MovieList added and Moive type as well as Nationality enum

--- DummyData.js
+++ DummyData.js
@@ -4,38 +4,90 @@ const UserList = [
       name: "John",
       username: "john",
       age: 20,
-      nationality: "Canada",  
+      nationality: "CANADA",
+      friends: [
+        {
+            id: 2,
+            name: "Oliver",
+            username: "Oliverhf",
+            age: 23,
+            nationality: "BRAZIL", 
+        },
+        {
+            id: 3,
+            name: "Sarah",
+            username: "sarah",
+            age: 25,
+            nationality: "UNITED_STATES",
+        },
+      ]  
     },
     {
         id: 2,
         name: "Oliver",
         username: "Oliverhf",
         age: 23,
-        nationality: "Brazil", 
+        nationality: "BRAZIL", 
     },
     {
         id: 3,
         name: "Sarah",
-        username: "cameron",
+        username: "sarah",
         age: 25,
-        nationality: "United States",
+        nationality: "UNITED_STATES",
+        friends: [
+            {
+                id: 2,
+                name: "Oliver",
+                username: "Oliverhf",
+                age: 23,
+                nationality: "BRAZIL", 
+            },
+        ]
     },
     {
         id: 4,
         name: "Julio",
         username: "julio123",
         age: 60,
-        nationality: "Germany",
+        nationality: "GERMANY",
     },
     {
         id: 5,
         name: "Gustavo",
         username: "gustavo2019",
         age: 5,
-        nationality: "Chile",  
+        nationality: "CHILE",  
     }
+]
 
+const MovieList = [
+    {
+        id: 1,
+        name: "Public Enemies",
+        yearOfPublication: 2009,
+        isInTheaters: false,
+    },
+    {
+        id: 2,
+        name: "Interstellar",
+        yearOfPublication: 2007,
+        isInTheaters: true,
+    },
+    {
+        id: 3,
+        name: "Superbad",
+        yearOfPublication: 2009,
+        isInTheaters: true,
+    },
+    {
+        id: 4,
+        name: "Oliver The Movie",
+        yearOfPublication: 2019,
+        isInTheaters: false,
+    },
+    
 
 ]
 
-module.exports = {UserList}
+module.exports = {UserList, MovieList}
